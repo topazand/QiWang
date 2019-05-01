20d73cb9d5d0d78040f8e250eac699e51572b7d3
topazand <49191786+topazand@users.noreply.github.com>
2019-05-01 14:35:10 -0400

    Add files via upload

---
+++ MadamWang.h
@@ -0,0 +1,53 @@
+#include <iostream>
+
+#ifndef I_Just_Say
+#define I_Just_Say cout
+#endif /* ifndef I_Just_Say */
+
+#ifndef I_Realize
+#define I_Realize cin
+#endif /* ifndef I_Realize */
+
+#ifndef Some_Students
+#define Some_Students if
+#endif /* ifndef Some_Students */
+
+#ifndef Other_Students
+#define Other_Students else
+#endif /* ifndef Other_Students */
+
+#ifndef Some_Other_Students
+#define Some_Other_Students else if
+#endif /* ifndef Some_Other_Students */
+
+#ifndef Manipulate
+#define Manipulate for
+#endif /* ifndef Manipulate */
+
+#ifndef Negotiate
+#define Negotiate while
+#endif /* ifndef Negotiate */
+
+#ifndef Reading_Quiz
+#define Reading_Quiz return
+#endif /* ifndef Reading_Quiz */
+
+#ifndef I_Am_Shocked
+#define I_Am_Shocked break
+#endif /* ifndef I_Am_Shocked */
+
+#ifndef Just_Copy
+#define Just_Copy =
+#endif /* ifndef Just_Copy */
+
+#ifndef Just
+#define Just ==
+#endif /* ifndef Just */
+
+#ifndef Not_Just
+#define Not_Just !=
+#endif /* ifndef Not_Just */
+
+#ifndef Class_Dismissed
+#define Class_Dismissed endl
+#endif /* ifndef Class_Dismissed */
---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include "MadamWang.h"
+using namespace std;
+
+int main(int argc, char **argv) {
+	// Insert code below.
+	int x;
+	I_Realize >> x;
+	Some_Students (x > 0) {
+		Negotiate (x Not_Just 1) {
+			Some_Students (x & 1) x Just_Copy x * 3 + 1;
+			Other_Students x >>Just_Copy 1;
+			I_Just_Say << x << Class_Dismissed;
+		}
+	} Some_Other_Students (x Just 0) {
+		I_Just_Say << "X is 0.\n";
+	} Other_Students {
+		Manipulate (int i Just_Copy 0; i < 5; i ++) {
+			Some_Students (i Just -x) I_Am_Shocked;
+			I_Just_Say << "Data error!\n";
+		}
+	}
+	Reading_Quiz 0;
+}
